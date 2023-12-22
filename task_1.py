d5b42772232d8e4464041242e3c89ba1a219d9bd
dashalovenorth <darya.vershinina.0505@mail.com>
2023-12-22 15:36:03 +0300

    Добавлен setup.cfg

--- task_1.py
+++ task_1.py
@@ -1,20 +1,20 @@
 """Calculates the three highest salaries and the percentage of all salaries to the top."""
 
-def statistics(*divisions: tuple[str, dict[str, float]], limit: float | None = None) -> tuple[str, dict[str, float]]:
+def statistics(*divisions: tuple[str, dict[str, float]], limit: float | None = None) -> tuple[str]:
     """Extract top 3 salaries and percentage.
 
     Args:
-        divisions: tuple(str, dict) - 
-        limit: float | None = None - 
+        divisions: tuple[str, dict[str, float]] - tuple of divisions names and wages
+        limit: float | None = None - limit from which wages are taken
 
     Returns:
-        tuple[str, dict[str, float]] -
+        tuple[list, float] - tuple of the three highest salaries and the percentage
     """    
+
 
     top_wages = []
     all_wages = []
     wages_limit = []
-
 
     for division in divisions:
         for wages in division[1].values():
@@ -26,8 +26,7 @@ def statistics(*divisions: tuple[str, dict[str, float]], limit: float | None = N
                 else:
                     wages_limit.append(wage)
 
-    top_wages = sorted(set(wages_limit), reverse = True)[:3] 
+    top_wages = sorted(set(wages_limit), reverse = True)[:3]
     percent = round(sum(top_wages) / sum(wages_limit) * 100, 2)
 
     return top_wages, percent
-
