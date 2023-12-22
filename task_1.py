a2bca48224c1dca1fefd3baf2c3aa01cc0798470
dashalovenorth <darya.vershinina.0505@mail.com>
2023-12-22 15:59:16 +0300

    Исправлены ошибки

--- task_1.py
+++ task_1.py
@@ -1,7 +1,7 @@
 """Calculates the three highest salaries and the percentage of all salaries to the top."""
 
 
-def statistics(*divisions: tuple[str, dict[str, float]], limit: float | None = None) -> tuple[str]:
+def statistics(*divisions: tuple[str, dict[str, float]], lim: float | None = None) -> tuple[str]:
     """Extract top 3 salaries and percentage.
 
     Args:
@@ -21,18 +21,13 @@ def statistics(*divisions: tuple[str, dict[str, float]], limit: float | None = N
             all_wages.append(wages)
 
     for wage in all_wages:
-        if limit is not None:
-            if wage >= limit:
+        if lim is not None:
+            if wage >= lim:
                 wages_limit.append(wage)
         else:
             wages_limit.append(wage)
 
-    top_wages = sorted(set(wages_limit), reverse=True)[:3]
+    top_wages = sorted(wages_limit, reverse=True)[:3]
     percent = round(sum(top_wages) / sum(wages_limit) * 100, 2)
 
     return top_wages, percent
-
-devision1 = ('ty', {'op': 657.0, 'pop': 345.0, 'tr': 7657.0})
-devision2 = ('pre', {'er': 67.0, 'eep': 3459.0, 'tr': 797.0})
-
-print(statistics(devision1, devision2, limit= 550.0))
