0a9a5f619c4f35f8ce702648d2450bee1cbac4f2
dashalovenorth <darya.vershinina.0505@mail.com>
2023-12-22 15:53:08 +0300

    Изменения

--- task_1.py
+++ task_1.py
@@ -19,14 +19,20 @@ def statistics(*divisions: tuple[str, dict[str, float]], limit: float | None = N
     for division in divisions:
         for wages in division[1].values():
             all_wages.append(wages)
-            for wage in all_wages:
-                if limit is not None:
-                    if wage >= limit:
-                        wages_limit.append(wage)
-                else:
-                    wages_limit.append(wage)
+
+    for wage in all_wages:
+        if limit is not None:
+            if wage >= limit:
+                wages_limit.append(wage)
+        else:
+            wages_limit.append(wage)
 
     top_wages = sorted(set(wages_limit), reverse=True)[:3]
     percent = round(sum(top_wages) / sum(wages_limit) * 100, 2)
 
     return top_wages, percent
+
+devision1 = ('ty', {'op': 657.0, 'pop': 345.0, 'tr': 7657.0})
+devision2 = ('pre', {'er': 67.0, 'eep': 3459.0, 'tr': 797.0})
+
+print(statistics(devision1, devision2, limit= 550.0))
