5dfbec394968d7476f6e84023e71f2e1119e567c
dashalovenorth <darya.vershinina.0505@mail.com>
2023-12-22 15:39:59 +0300

    Изменения по pep8

--- task_1.py
+++ task_1.py
@@ -1,16 +1,16 @@
 """Calculates the three highest salaries and the percentage of all salaries to the top."""
 
+
 def statistics(*divisions: tuple[str, dict[str, float]], limit: float | None = None) -> tuple[str]:
     """Extract top 3 salaries and percentage.
 
     Args:
-        divisions: tuple[str, dict[str, float]] - tuple of divisions names and wages
-        limit: float | None = None - limit from which wages are taken
+        divisions: tuple[str, dict[str, float]] - tuple of divisions names and wages.
+        limit: float | None = None - limit from which wages are taken.
 
     Returns:
-        tuple[list, float] - tuple of the three highest salaries and the percentage
-    """    
-
+        tuple[list, float] - tuple of the three highest salaries and the percentage.
+    """
 
     top_wages = []
     all_wages = []
@@ -26,7 +26,7 @@ def statistics(*divisions: tuple[str, dict[str, float]], limit: float | None = N
                 else:
                     wages_limit.append(wage)
 
-    top_wages = sorted(set(wages_limit), reverse = True)[:3]
+    top_wages = sorted(set(wages_limit), reverse=True)[:3]
     percent = round(sum(top_wages) / sum(wages_limit) * 100, 2)
 
     return top_wages, percent
